15f8d09730399193b9b66aeed8ac1eeb9f7b763c
jjlmoya <juanjo1989@gmail.com>
2026-04-11 13:35:01 +0200

    feat: migration music library

--- src/index.ts
+++ src/index.ts
@@ -17,3 +17,10 @@ export type {
 
 export { ALL_TOOLS } from './tools';
 
+export { METRONOME_TOOL } from './tool/metronome';
+export { BPM_CALCULATOR_TOOL } from './tool/bpmCalculator';
+export { PHASE_DISTANCE_TOOL } from './tool/phaseDistance';
+export { FREQUENCY_NOTE_CONVERTER_TOOL } from './tool/frequencyNoteConverter';
+export { AUDIO_STORAGE_CALCULATOR_TOOL } from './tool/audioStorageCalculator';
+export { MUSIC_NOTE_EQUIVALENCY_TOOL } from './tool/musicNoteEquivalency';
+
