26e6ba6e86b2345fb736e070841bbcbfbdfe28da
jjlmoya <juanjo1989@gmail.com>
2026-04-18 19:40:47 +0200

    feat: add ALL_ENTRIES export

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ export type {
   ToolDefinition,
 } from './types';
 
-export { ALL_TOOLS } from './tools';
+export { ALL_ENTRIES, ALL_TOOLS } from './tools';
 
 export { METRONOME_TOOL } from './tool/metronome';
 export { BPM_CALCULATOR_TOOL } from './tool/bpmCalculator';
